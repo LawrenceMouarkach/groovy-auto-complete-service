de359772ff15167126d3a3eefd01211228c9af3d
lawrence.mouarkach <lawrence.mouarkach@yodel.co.uk>
2018-08-20 15:17:03 +0100

    added dynamic button rendering

--- src/main/resources/js/auto-complete.js
+++ src/main/resources/js/auto-complete.js
@@ -8,10 +8,15 @@ function autoComplete() {
             table.append("<tbody>")
             var tr;
             for (var i = 0; i < result.length; i++) {
+                var buttonTitle = result[i].iata_code
+                var searchQuery = result[i].name
+                var navigate = "parent.open('https://www.google.co.uk/search?q="+searchQuery+"&oq="+ searchQuery+"')"
                 tr = $("<tr>");
                 tr.append("<td>" + result[i].type+ "</td>");
                 tr.append("<td>" + result[i].name + "</td>");
-                tr.append("<td>" + result[i].iata_code + "</td>");
+                tr.append("<td>" + "<button name='table-row-button-"+i+"'"+
+                "onclick="+ '"'+ navigate + '" '+
+                "class='btn btn-sm btn-success' type='submit' value='"+buttonTitle+"'>"+buttonTitle+"</button>" +"</td>");
                 tr.append("</tr>");
                 table.append(tr);
             }
